4e8c3a7d3e8dd28fd6b3ff3896ba4b81a2f297df
AliMarwana <64220279+AliMarwana@users.noreply.github.com>
2025-10-25 11:23:30 +0100

    Change Filters_applied to Dictionary type

--- Task1/DTOs/FilteredStringsDto.cs
+++ Task1/DTOs/FilteredStringsDto.cs
@@ -4,7 +4,7 @@
     {
         public List<ReturnCreateStringDto> Data { get; set; } = new List<ReturnCreateStringDto>();
         public int Count{ get; set; }
-        public FiltersApplied? Filters_applied { get; set; }
+       public Dictionary<string, object>? Filters_applied { get; set; }
 
     }
 }
